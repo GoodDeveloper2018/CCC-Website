8a31d9895d1c3dbef2dad1063debe8e641fbccac
Arsh <80232792+GoodDeveloper2018@users.noreply.github.com>
2024-11-23 17:25:13 -0500

    Update Resources.js
    Routed onClick functions to resource links for members of Club

--- src/Resources.js
+++ src/Resources.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
 function Resources() {
+  const handleBookClick = () => {
+    window.open('https://darrenyao.com/usacobook/cpp.pdf#page=12', '_blank');
+  };
+
+  const handleDSClick = () => {
+    window.open('https://usaco.guide/bronze/intro-ds?lang=cpp', '_blank');
+  };
+
+  const handleAlgoClick = () => {
+    window.open('https://www.geeksforgeeks.org/top-algorithms-and-data-structures-for-competitive-programming/', '_blank');
+  };
+
+  const handlePracticeClick = () => {
+    window.open('https://usaco.guide/problems/', '_blank');
+  };
+
   return (
     <div className="page-container">
       <h1>Learning Resources</h1>
       <div className="content">
         <h2>Competitive Programming Resources</h2>
         <ul>
-          <li>Algorithm Tutorials</li>
-          <li>Data Structures Guide</li>
-          <li>Practice Platforms</li>
-          <li>Recommended Books</li>
+          <li onClick={handleAlgoClick} style={{ cursor: 'pointer' }}>Algorithm Tutorials</li>
+          <li onClick={handleDSClick} style={{ cursor: 'pointer' }}>Data Structures Guide</li>
+          <li onClick={handlePracticeClick} style={{ cursor: 'pointer' }}>Practice Platforms</li>
+          <li onClick={handleBookClick} style={{ cursor: 'pointer' }}>Recommended Books</li>
         </ul>
       </div>
     </div>
